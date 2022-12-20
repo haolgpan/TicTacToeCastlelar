54051ed69d6df7863559931fed05a79f36a5ed5a
haolgpan <haolgpan@gmail.com>
2022-12-20 20:26:28 +0100

    First Commit

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+module TicTacToeCastlelar {
+    requires javafx.graphics;
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires java.prefs;
+    //exports main.tictactoe;
+    opens main.tictactoe.view;
+
+}
