7db777647189bb3485421aa9cb04b7da0aa8ff4f
Hao <haolgpan@gmail.com>
2023-01-08 17:39:44 +0100

    Updating

---
+++ src/main/tictactoe/view/GameInterfaceController.java
@@ -0,0 +1,20 @@
+package main.tictactoe.view;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.ResourceBundle;
+
+public class GameInterfaceController implements Initializable {
+    Random random = new Random();
+
+    ArrayList<Button> buttons;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
